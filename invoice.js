04bb7d7508aa91a1fb8438e90a6f09f46cdc77bc
Jeroen Slemmer <jslemmer@davinci.nl>
2024-12-19 12:17:19 +0100

    services invoer

--- invoice.js
+++ invoice.js
@@ -22,13 +22,39 @@ function showSection(activateSection){
     activateSection.classList.add('active');
 }
 
+function createIntegerInput(max, input){
+    if (!input){
+        const input = document.createElement('input');
+    }
+    if (max){
+        input.maxValue = max;
+    } else {
+        input.maxValue = 600;
+    }
+    input.addEventListener('input', function (e) {
+        this.value = this.value.replace(/[^0-9]/g, '');
+    });
+    input.addEventListener('blur', function (e) {
+        
+        const numberValue = parseInt(this.value, 10);
+        if (numberValue > this.maxValue) {
+            alert('Klopt dit?');
+        }
+    });
+    return input;
+}
+
 function createLessonDate(after){
     const lessonDate = document.querySelector('.lessonDateTemplate');
     const clone = lessonDate.cloneNode(true);
+    for (input of clone.querySelectorAll('.int')){
+        input = createIntegerInput(300,input);
+    }
     const date = clone.querySelector('[type="date"]');
     date.value = '';
     const createButton = clone.querySelector('.createLessonDate');
     clone.classList.remove('lessonDateTemplate');
+    clone.classList.add('lessonDate');
     const lessonData = document.getElementById('lessonData');
     if (after && after.nextSibling){
         lessonData.insertBefore(clone, after.nextSibling)
@@ -36,10 +62,35 @@ function createLessonDate(after){
         lessonData.appendChild(clone);
     }
     createButton.onclick = (e) => {createLessonDate(clone)};
+    const buttonCreateService = clone.querySelector('.createService');
+    const services = clone.querySelector('.services');
+    buttonCreateService.onclick = (e) => {createService(services)}
+
 }
 createLessonDate();
 
-const buttonCreateLessonDate = document.getElementById('createLessonDate');
+// const buttonCreateLessonDate = document.getElementById('createLessonDate');
+
+
+function createService(services,after){
+    const service = document.querySelector('.serviceTemplate');
+    const clone = service.cloneNode(true);
+    
+    clone.classList.remove('serviceTemplate');
+    clone.classList.add('service');
+
+    if (after && after.nextSibling){
+        services.insertBefore(clone, after.nextSibling)
+    } else {
+        services.appendChild(clone);
+    }
+
+    const removeButton = clone.querySelector('.removeService');
+    removeButton.onclick = (e) => {clone.remove();}
+}
+
+
+
 
 
 
